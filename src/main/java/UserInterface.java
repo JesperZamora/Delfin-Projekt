533e07325a626cf65cd624b5e8fa446c2e1f171a
mathiasfri <mathiasfridthjof@live.dk>
2022-11-23 12:16:51 +0100

    interface update

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -10,10 +10,14 @@ public class UserInterface {
 
     public void startMenu(){
         System.out.println("""
-                Delfin program
-                1. add member
-                2. view member
-                9. quit program""");
+                Welcome to the interface of the dolphin swimming club!
+                Make your choice:
+                
+                1. President menu
+                2. Cashier menu
+                3. Coach menu
+                9. Quit
+                """);
     }
 
     public void startMenuCommand(){
@@ -22,11 +26,57 @@ public class UserInterface {
             startMenu();
             int userChoice = readInteger();
             switch(userChoice) {
-                case 1 -> addMember();
-                case 2 -> viewMember();
+                case 1 -> presidentMenu();
+                case 2 -> cashierMenu();
+                case 3 -> coachMenu();
                 case 9 -> isRunning = false;
                 default -> System.out.println("Not valid menu choice");
             }
+        }
+    }
+
+    public void presidentMenu(){
+        System.out.println("""
+                1. Add member
+                2. Edit member
+                3. Show members
+                """);
+        int userChoice = readInteger();
+        switch(userChoice) {
+            case 1 -> System.out.println(" *** ADD MEMBER TO BE ADDED HERE ***");
+            case 2 -> System.out.println(" *** EDIT MEMBER TO BE ADDED HERE ***");
+            case 3 -> System.out.println(" *** SHOW MEMBERS TO BE ADDED HERE ***");
+            default -> System.out.println("Wrong input");
+        }
+    }
+
+    public void cashierMenu(){
+        System.out.println("""
+                1. Show subscriptions
+                2. Show arrears
+                3. Sum of arrears
+                """);
+        int userChoice = readInteger();
+        switch (userChoice){
+            case 1 -> System.out.println(" *** SHOW SUBSCRIPTION TO BE ADDED HERE ***");
+            case 2 -> System.out.println(" *** ARREARS TO BE ADDED HERE ***");
+            case 3 -> System.out.println(" *** SUM OF ARREARS TO BE ADDED HERE ***");
+            default -> System.out.println("Wrong input");
+        }
+    }
+
+    public void coachMenu(){
+        System.out.println("""
+                1. Show division of competitive swimmers
+                2. Register competitive swimmers to specific disciplines
+                3. Show top 5 swimmers in each discipline
+                """);
+        int userChoice = readInteger();
+        switch (userChoice){
+            case 1 -> System.out.println(" *** DIVISION OF COMPETITIVE SWIMMERS TO BE ADDED HERE ***");
+            case 2 -> System.out.println(" *** COMPETITIVE SWIMMERS REGISTERED TO SPECIFIC DISCIPLINES TO BE ADDED HERE ***");
+            case 4 -> System.out.println(" *** TOP 5 IN EACH DISCIPLINE TO BE ADDED HERE ***");
+            default -> System.out.println("Wrong input");
         }
     }
 
