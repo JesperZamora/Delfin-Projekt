244e26be652608a586496ddfe43332b9e76ff8e1
JesperZamora <jesp78m6@stud.kea.dk>
2022-11-23 11:11:19 +0100

    menu + switch added + readInteger method

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class UserInterface {
+    private Scanner sc;
+    private Controller controller;
+    public UserInterface(){
+        sc = new Scanner(System.in);
+        controller = new Controller();
+    }
+
+    public void startMenu(){
+        System.out.println("""
+                Delfin program
+                1. add member
+                2. view member
+                9. quit program""");
+    }
+
+    public void startMenuCommand(){
+        boolean isRunning = true;
+        while(isRunning) {
+            startMenu();
+            int userChoice = readInteger();
+            switch(userChoice) {
+                case 1 -> addMember();
+                case 2 -> viewMember();
+                case 9 -> isRunning = false;
+                default -> System.out.println("Not valid menu choice");
+            }
+        }
+    }
+
+    public void addMember(){
+        System.out.println("not working yet");
+    }
+    public void viewMember() {
+        System.out.println("not working yet");
+    }
+
+    public int readInteger(){
+        while(!sc.hasNextInt()){
+            System.out.println("This is not a number. Try again..");
+        }
+        return sc.nextInt();
+    }
 }
