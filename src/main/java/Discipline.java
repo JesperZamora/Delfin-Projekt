09abe3fc483e0ce6bd7b771b6e239375d8deb5ae
Maloulou99 <baha0610@stud.kea.dk>
2022-11-29 14:41:04 +0100

    Tilføjet klasser

---
+++ src/main/java/Discipline.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+public class Discipline  {
+
+    private String butterfly;
+    private String crawl;
+    private String rygcrawl;
+    private String brystsvømning;
+
+    public Discipline(String butterfly, String crawl, String rygcrawl, String brystsvømning){
+        this.butterfly = butterfly;
+        this.crawl = crawl;
+        this.rygcrawl = rygcrawl;
+        this.brystsvømning = brystsvømning;
+    }
+
+
+
+}
