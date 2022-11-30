ab87b5fb3d41fa9454c9e932617a4ae0868f5f68
JesperZamora <jesp78m6@stud.kea.dk>
2022-11-30 12:00:44 +0100

    Discipline construktor + add methode made. The add methode will also create a new instance of its class and set the new instance in a free spot on the disciplines

--- src/main/java/Competition.java
+++ src/main/java/Competition.java
@@ -1,12 +1,12 @@
-import java.time.LocalDate;
-
 public class Competition {
-    int placement;
-    String competitionName;
-    int time;
+    private int placement;
+    private String competitionName;
+    private int time;
     public Competition(String competitionName, int placement, int time) {
         this.competitionName = competitionName;
         this.placement = placement;
         this.time = time;
     }
+
+
 }
--- src/main/java/Discipline.java
+++ src/main/java/Discipline.java
@@ -2,16 +2,41 @@ import java.time.LocalDate;
 
 public class Discipline  {
 
-    private String butterfly;
-    private String crawl;
-    private String rygcrawl;
-    private String brystsvømning;
+    private String disciplineName;
+    private String trainOrCompResult;
+    private LocalDate date;
+    private double time;
+    private Discipline[] disciplines = new Discipline[4];
 
-    public Discipline(String butterfly, String crawl, String rygcrawl, String brystsvømning){
-        this.butterfly = butterfly;
-        this.crawl = crawl;
-        this.rygcrawl = rygcrawl;
-        this.brystsvømning = brystsvømning;
+    public Discipline(String disciplineName, String trainOrCompResult, LocalDate date, double time) {
+        this.disciplineName = disciplineName;
+        this.trainOrCompResult = trainOrCompResult;
+        this.date = date;
+        this.time = time;
     }
 
+    public void addDisciplines(String disciplineName, String trainOrCompResult, LocalDate date, double time) {
+        for(int i = 0; i < disciplines.length; i++) {
+            if(disciplines[i] == null) {
+                disciplines[i] = new Discipline(disciplineName, trainOrCompResult, date, time);;
+                break;
+            }
+        }
+    }
+
+    public void setDisciplineName(String disciplineName) {
+        this.disciplineName = disciplineName;
+    }
+
+    public void setTrainOrCompResult(String trainOrCompName) {
+        this.trainOrCompResult = trainOrCompName;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
 }
