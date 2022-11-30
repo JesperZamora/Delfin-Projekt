9f7d55b1a7108d9785e7418392864b9944045f91
OsamaMansour25 <osam0289@stud.kea.dk>
2022-11-30 09:44:56 +0100

    Første Træner userStory done

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -6,44 +6,47 @@ public class Controller {
     private FileHandler fileHandler;
     private Team team;
 
-    public Controller(){
+    public Controller() {
         database = new Database();
         fileHandler = new FileHandler();
 
     }
 
     public void addExerciser(String name, LocalDate birthDate, int phoneNumber, String address,
-                             String memberStatus){
+                             String memberStatus) {
         database.addExerciser(name, birthDate, phoneNumber, address, memberStatus);
     }
 
     public void addCompetitionSwimmer(String name, LocalDate birthDate, int phoneNumber, String address,
-                                      String memberStatus){
+                                      String memberStatus) {
         database.addCompetitionSwimmer(name, birthDate, phoneNumber, address, memberStatus);
     }
 
-    public ArrayList<Member> getMembers(){
+    public ArrayList<Member> getMembers() {
         return database.getMembers();
     }
 
-    public ArrayList<Member> searchMember(String searchWord){
+    public ArrayList<Member> searchMember(String searchWord) {
         return database.searchMember(searchWord);
     }
 
-    public void saveFile(){
-       fileHandler.saveFile(getMembers());
+    public void saveFile() {
+        fileHandler.saveFile(getMembers());
     }
 
-    public void loadFile(){
+    public void loadFile() {
         database.loadFile(fileHandler.loadFile());
     }
-public void sortCompGroupByAge() {
+
+    public void sortCompGroupByAge() {
         team.sortCompMembersAge();
-}
-public ArrayList<Member> getCompMembersOver18() {
+    }
+
+    public ArrayList<Member> getCompMembersOver18() {
         return team.getGetCompMembersOver18();
-}
-public ArrayList<Member> getCompMembersUnder18() {
+    }
+
+    public ArrayList<Member> getCompMembersUnder18() {
         return team.getCompMembersUnder18();
-}
+    }
 }
