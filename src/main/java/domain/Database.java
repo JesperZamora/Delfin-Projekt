aaf23f5c56fc0a8b1b41c5d89e314c77f3564d53
OsamaMansour25 <osam0289@stud.kea.dk>
2022-12-07 19:55:17 +0100

    Rettet sum

--- src/main/java/domain/Database.java
+++ src/main/java/domain/Database.java
@@ -40,7 +40,6 @@ public class Database {
     public int sumOfSubscriptions(ArrayList<Member> member){
         int sum = 0;
         for (Member m : member) {
-            if (m.isPaid())
                 sum += m.getSubscriptionPrice();
         }
         return sum;
